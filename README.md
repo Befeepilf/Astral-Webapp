d915b7c01f61582adcdccad061ce3bc332795f9d
Befeepilf <befeepilf@protonmail.com>
2020-06-08 14:46:00 +0200

    set up of nextjs

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Astral-Webapp
+
+An implementation of [Ivan Zagorik's beautiful design project](https://www.behance.net/gallery/79555113/Astral-Cinema?tracking_source=search_projects_recommended%7Cmovie)
---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import Layout from 'components/Layout.jsx';
+
+export default function Home() {
+  return (
+    <Layout>
+      <Head>
+        <title>Astral</title>
+      </Head>
+
+      <style jsx>{`
+      `}</style>
+    </Layout>
+  );
+}
